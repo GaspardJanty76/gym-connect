c1d14421c9ea5fa913896b92f21f89a6020b8892
Gaspard Janty <144214357+GaspardJanty76@users.noreply.github.com>
2023-11-15 21:24:21 +0100

    V1
    création du menu et de la PWA

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css">
+    <link rel="manifest" href="json/manifest.json">
+    <link rel="icon" type="image/png" href="img/gcicon.jpg"/>
+    <title>Navbar Responsive</title>
+</head>
+<body>
+<nav class="nav">
+  <input id="menu" type="checkbox">
+  <label class="menu" for="menu"><i class="fa-solid fa-bars fa-xl" aria-hidden="true"></i></label>
+  <ul class="menu">
+    <li>
+      <a href="#0">
+        <span>Accueil</span>
+        <i class="fa-solid fa-dumbbell" aria-hidden="true"></i>
+      </a>
+    </li>
+    <li>
+      <a href="#0">
+        <span>Profil</span>
+        <i class="fas fa-address-card" aria-hidden="true"></i>
+      </a>
+    </li>
+    <li>
+      <a href="#0">
+        <span>Amis</span>
+        <i class="fas fa-users" aria-hidden="true"></i>
+      </a>
+    </li>
+    <!-- <li>
+      <a href="#0">
+        <span>Contact</span>
+        <i class="fas fa-envelope-open-text" aria-hidden="true"></i>
+      </a>
+    </li> -->
+  </ul>
+</nav>
+
+
+</body>
+</html>
